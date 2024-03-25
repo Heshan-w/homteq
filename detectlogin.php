a7e690446f86a6cc95bfba68b5a098feba09b459
Heshan-w <htwanigasinghe@gmail.com>
2024-03-25 23:39:51 +0530

    initial commit

---
+++ detectlogin.php
@@ -0,0 +1,15 @@
+<?php
+// Start the session
+session_start();
+
+// Check if the user is logged in
+if (isset($_SESSION['userid'])) {
+    // If logged in, display the user's full name
+    echo "<p>Welcome, " . $_SESSION['fname'] . " " . $_SESSION['sname'] . "</p>";
+} else {
+    // If not logged in, redirect to the login page or display a message
+    // For example:
+    // header("Location: login.php");
+    // echo "Please log in to access this page.";
+}
+?>
